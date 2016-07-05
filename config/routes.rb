1a1931a75ad4b454be503232969ffdc05d4ac160
Phil Spitzer <thespitzer@gmail.com>
2016-07-05 22:19:44 +0000

    oauth try 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   get 'users/new'
 
   root 'static_pages#home'
   get  '/about',   to: 'static_pages#about'
   get  '/signup',  to: 'users#new'
+  
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+
+  resources :sessions, only: [:create, :destroy]
 end
