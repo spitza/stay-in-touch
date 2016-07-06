56646ad72a955ff6a0c6fc18c700e75bc8064e70
Phil Spitzer <thespitzer@gmail.com>
2016-07-06 22:17:01 +0000

    new form with select

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160706200405) do
+ActiveRecord::Schema.define(version: 20160706214225) do
 
   create_table "meetings", force: :cascade do |t|
     t.text     "content"
     t.integer  "user_id"
     t.string   "invitee"
-    t.boolean  "work",       default: false
     t.integer  "frequency"
     t.boolean  "active"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "type"
     t.index ["user_id", "created_at"], name: "index_meetings_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_meetings_on_user_id"
   end
