835ea46ed98f0c91061fe31f4ad8930d875dbb2a
Phil Spitzer <thespitzer@gmail.com>
2016-07-06 17:48:26 +0000

    accurately linking meetings w users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :microposts, dependent: :destroy
+  has_many :meetings, dependent: :destroy
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
       user.provider = auth.provider
