20cd1586ca680a6a2f8218c91c8d4dc7d5f71bdc
Phil Spitzer <thespitzer@gmail.com>
2016-07-07 19:07:45 +0000

    trying out sending an email

--- Gemfile
+++ Gemfile
@@ -16,7 +16,8 @@ gem 'coffee-rails',            '4.2.1'
 gem 'jquery-rails',            '4.1.1'
 gem 'turbolinks',              '5.0.0'
 gem 'jbuilder',                '2.4.1'
-gem "omniauth-google-oauth2"
+gem 'omniauth-google-oauth2'
+gem 'rest-client'
 
 group :development, :test do
   gem 'sqlite3', '1.3.11'
--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -4,13 +4,12 @@ class MeetingsController < ApplicationController
   def create
     @meeting = current_user.meetings.build(meeting_params)
     if @meeting.save
-      flash[:success] = "Nice! We sent them an email so you can stay in touch."
-      RestClient.post "https://api:key-2462c9d10fd68bcea7e60464e17e7fad"\
-        "@api.mailgun.net/v3/sandbox962f74bced9b4f80b260c206f022bf53.mailgun.org/messages",
-        :from => "Mailgun Sandbox <postmaster@sandbox962f74bced9b4f80b260c206f022bf53.mailgun.org>",
-        :to => "Phil <thespitzer@gmail.com>",
-        :subject => "Hello Phil",
-        :text => "Congratulations Phil, you just sent an email with Mailgun!  You are truly awesome!  You can see a record of this email in your logs: https://mailgun.com/cp/log .  You can send up to 300 emails/day from this sandbox server.  Next, you should add your own domain so you can send 10,000 emails/month for free."
+      RestClient.post "https://api:#{ENV["MAILGUN_API_KEY"]}"\
+      "@api.mailgun.net/v3/mg.stayintouch.link/messages",
+      :from => "Stay in Tizouch <mailgun@mg.stayintouch.link>",
+      :to => "thespitzer@gmail.com, phil@mg.stayintouch.link",
+      :subject => "Hello phillll",
+      :text => "Testing some Mailgun awesomness yuip yupyup !"
       redirect_to root_url
     else
       render 'static_pages/home'
